3ebfc47a1c301f26b26003609cf042121d6077d2
Anali Estrada Sandoval <analiestrada44@gmail.com>
2023-05-23 17:21:43 -0700

    array map lab complete

--- index.js
+++ index.js
@@ -11,6 +11,47 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+// // const arr1 = tutorials[0].split(' ');
+// if (tutorials) {
+//   return tutorials.map(function(tutorial){
+//   return tutorial.split(' ')
+// })
+// }
+// if (tutorials) {
+//   return tutorials.map(function(tutorial){
+//     return tutorial.charAt(0).toUpperCase() + tutorial.slice(1)
+//   })
+// }
+// return tutorials.join(' ')
+// }
+
+
+// const titleCased = () => {
+//   if (tutorials) {
+//    return tutorials.map(function(tutorial){
+//    return tutorial.split(' ')
+//  } 
+//  }
+
+// let newArray = tutorials[0].split(' ')
+// console.log(newArray[0][0])
+
+// function titleCased(tutorials){
+//   return tutorials.toLowerCase().split(' ').map(
+//   function(word){
+//   return (word.chartAt(0).toUpperCase() + word.slice(1));
+//   }).join(' ');
+// }
+
 const titleCased = () => {
-  return tutorials
+  const newArray = [];
+  tutorials.map(function(elemet){
+    const words = elemet.split(" ");
+    const capitalizer = words.map(function(word){
+      return word[0].toUpperCase() + word.substring(1)
+    }).join(" ");
+    newArray.push(capitalizer);
+  })
+  return newArray;
 }
